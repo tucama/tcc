b350b67cc8023f7d819b4a308e694096fbd669aa
Tucamar <xxtucamarxx@hotmail.com>
2024-07-18 09:02:28 -0300

    clean automatize.sh
    removing redundancies

--- dynamics/scripts/automatize.sh
+++ dynamics/scripts/automatize.sh
@@ -13,26 +13,32 @@ namd_conf="${script_dir}/namd.conf"
 
 rnr "_final" "" "$data_dir" -f -r --no-dump
 # Find all .pdb files
-fd -a -e "pdb" . "${data_dir}" | while read -r file; do
-    # Extract the file name
-    file_name=$(basename "$file")
-    
-    pdb="${file_name:0:4}"
-    # Extract the desired directory structure from the file name
-    dir_name=$(echo "$file_name" | sed -E 's/^(.{4}_[^_]*_[^_]*)_.*/\1/')
+echo "creating min dir"
+# fd -a -e "pdb" . "${data_dir}" | while read -r file; do
+#     # Extract the file name
+#     file_name=$(basename "$file")
+#     
+#     pdb="${file_name:0:4}"
+#
+#     # Create the new directory structure
+#     out_dir="$min_dir/$pdb/${file_name%.*}"
+#     out_file="${out_dir}/${file_name}"
+#     tcl_script="${out_dir}/${file_name%.*}_auto.tcl"
+#     
+#     # Copy the file to the new directory
+#     if [ ! -f $out_file ] || [ ! -f $tcl_script ] ; then
+#         mkdir -p "$out_dir"
+#         cp "$file" "$out_file"
+#         cp "${script_dir}/prepare_pdbs.tcl" "${tcl_script}"
+#         sd "INPUT" "${out_file}" "${tcl_script}" 
+#         sd "SSBOND" "set ssbond_script ${ssbond_script}" "${tcl_script}" 
+#         sd "TOPOLOGY" "topology ${top_file}" "${tcl_script}" 
+#         sd "NAMD_CONF" "${namd_conf}" "${tcl_script}" 
+#     fi
+# done
 
-    # Create the new directory structure
-    out_dir="$min_dir/$pdb/$dir_name"
-    out_file="${out_dir}/${file_name}"
-    mkdir -p "$out_dir"
-    tcl_script="${out_dir}/${dir_name}_auto.tcl"
-    
-    # Copy the file to the new directory
-    cp "$file" "$out_file"
-    cp "${script_dir}/prepare_pdbs.tcl" "${tcl_script}"
-    sd "INPUT" "${out_file}" "${tcl_script}" 
-    sd "SSBOND" "set ssbond_script ${ssbond_script}" "${tcl_script}" 
-    sd "TOPOLOGY" "topology ${top_file}" "${tcl_script}" 
-    sd "NAMD_CONF" "${namd_conf}" "${tcl_script}" 
+fd -a -e "tcl" . "${min_dir}" | while read -r file; do
+    cd $(dirname ${file})
+    # vmd -dispdev text -e $file
+    echo "creating psf files for ${file}"
 done
-
