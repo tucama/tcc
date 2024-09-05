03df85b2292f64029214add82d67d7470161f252
Tucamar <xxtucamarxx@hotmail.com>
2024-09-04 21:58:21 -0300

    updated mopac keywords

--- dynamics/scripts/pdbtomop.sh
+++ dynamics/scripts/pdbtomop.sh
@@ -1,5 +1,4 @@
 dynamic_dir="$HOME/Documents/tcc/dynamics"
 mopac_dir="${dynamic_dir}/2_mopac"
-dir="${1:-$mopac_dir}"
 
-fd . "${dir}" -e "pdb" -t f -x echo {.} | xargs -I {} bash -c 'echo -e "PM7 AUX LARGE MOZYME PDB ALLVECS VECTORS EPS=78.4 1SCF\n\n" | cat - {}.pdb > {}.tmp && mv {}.tmp {}.mop && rm {}.pdb'
+fd . "${1:-$mopac_dir}" -e "pdb" -t f -x echo {.} | xargs -I {} bash -c 'echo -e "PM7 AUX LARGE ALLVECS VECTORS GEO-OK 1SCF MOZYME XYZ PL T=1D EPS=78.4 RSOLV=1.3 PDB CUTOFF=9.0 LET DISP(1.0)\n\n" | cat - {}.pdb > {}.tmp && mv {}.tmp {}.mop && rm {}.pdb'
