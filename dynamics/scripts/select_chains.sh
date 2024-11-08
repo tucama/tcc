d9e73720e88ad2fd2763f5d2c614676fb5b247e2
tucama <xxtucamarxx@hotmail.com>
2024-11-08 12:33:44 -0300

    improve select_chain scrpit

--- dynamics/scripts/select_chains.sh
+++ dynamics/scripts/select_chains.sh
@@ -9,37 +9,44 @@ fd ${1:-.} "./1_min/" -a -t f -e "pdb" | while read -r file; do
     dirname=$(dirname "$file")
     pdb=${filename:0:4}
 
-    chains=$(echo "$file" | perl -ne 'if (/_(\w{1,2})_(\w{1,2})\..+/) { print "$1 $2\n"; }')
+    # Extract chain information
+    chains=$(echo "$file" | perl -ne 'if (/_([A,B]+)_([B,C,D,E]+)\..+/) { print "$1 $2\n"; }')
     first=$(echo "$chains" | awk '{print $1}')
     second=$(echo "$chains" | awk '{print $2}')
 
+    echo "$first and $second for $filename"
+    # Format chain names if necessary
     if [ ${#first} -eq 2 ]; then
         first="${first:0:1},${first:1:1}"
     fi
     if [ ${#second} -eq 2 ]; then
         second="${second:0:1},${second:1:1}"
     fi
-    cd $dirname
+    echo "$first and $second for $filename"
 
+    # Process the file if it contains "wt" (wild type)
     if [[ ${filename} == *"wt"* ]]; then
-        # echo "wild type file, Extracting both chains"
-        echo $file
-        pdb_selchain -${first} ${file} | perl -00 -pe 's/TER\nTER\n//' > "${dirname}/${pdb}_${first//,/}.pdb"
-        pdb_selchain -${second} ${file} | perl -00 -pe 's/TER\nTER\n//' > "${dirname}/${pdb}_${second//,/}.pdb"
+        # Wild-type file, extracting both chains
+        echo "Processing wild type file: $file"
+        echo "pdb_selchain -${first} "$file" | perl -00 -pe 's/TER\nTER\n//' > "${dirname}/${pdb}_${first//,/}.pdb""
+        pdb_selchain -${first} "$file" | perl -00 -pe 's/TER\nTER\n//' > "${dirname}/${pdb}_${first//,/}.pdb"
+        pdb_selchain -${second} "$file" | perl -00 -pe 's/TER\nTER\n//' > "${dirname}/${pdb}_${second//,/}.pdb"
         
     else
-        # Extract the 6th character from the filename
+        # Extract mutation information
         mutation=$(echo "$filename" | cut -c 6)
         mut_pdb=$(echo "$filename" | sed -E 's/^(([^_]*_){2}[^_]*).*/\1/')
 
+        # Select the appropriate chain based on mutation
         if [[ "${first}" == *"${mutation}"* ]]; then
-        echo $file
-            pdb_selchain -${first} ${file} > "${dirname}/${mut_pdb}.pdb"
-            grep -v "TER" "${dirname}/${mut_pdb}.pdb" > "temp_file" && mv "temp_file" "${dirname}/${mut_pdb}.pdb"
+            echo "Processing file: $file (chain $first)"
+            pdb_selchain -${first} "$file" > "${dirname}/${mut_pdb}.pdb"
         else
-        echo $file
-            pdb_selchain -${second} ${file} > "${dirname}/${mut_pdb}.pdb"
-            grep -v "TER" "${dirname}/${mut_pdb}.pdb" > "temp_file" && mv "temp_file" "${dirname}/${mut_pdb}.pdb"
+            echo "Processing file: $file (chain $second)"
+            pdb_selchain -${second} "$file" > "${dirname}/${mut_pdb}.pdb"
         fi
+
+        # Clean up the PDB file by removing "TER" lines
+        grep -v "TER" "${dirname}/${mut_pdb}.pdb" > "${dirname}/${mut_pdb}.pdb.tmp" && mv "${dirname}/${mut_pdb}.pdb.tmp" "${dirname}/${mut_pdb}.pdb"
     fi
 done
